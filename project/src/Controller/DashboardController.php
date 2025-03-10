1459548b5a180da5bff33f1ba529d2f073f86fc1
Florian Lavigne Chappaz <florian.lavigne.chappaz@gmail.com>
2025-03-10 11:50:31 +0100

    add edit schedule function + migration (delete VichBundle fields)

--- project/src/Controller/DashboardController.php
+++ project/src/Controller/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Schedule;
 use App\Form\ScheduleType;
+use App\Repository\ScheduleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -42,6 +43,27 @@ final class DashboardController extends AbstractController
         return $this->render('schedule/create.html.twig', [
             'controller_name' => 'DashboardController',
             'form' => $form,
+        ]);
+    }
+
+    #[Route('/dashboard/{id}/edit', name: 'app_dashboard_edit', requirements: ['id' => '\d+'])]
+    public function edit(ScheduleRepository $scheduleRepository, Request $request, EntityManagerInterface $em, int $id): Response
+    {
+        $schedule = $scheduleRepository->find($id);
+
+        $form = $this->createForm(ScheduleType::class, $schedule); // création d'un formulaire avec la récupération du formulaire "type"
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($schedule); // prépare/mets en attente les données avant de les insérer ou update dans la bdd
+            $em->flush(); // éxécute réellement les requêtes SQL en attente
+
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+        return $this->render('schedule/edit.html.twig', [
+            'schedule' => $schedule,
+            'form' => $form->createView(),
         ]);
     }
 }
